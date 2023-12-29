f9c066e6fbd0feb266e0fa400a2bbe776ead81d9
Daniel <danieldiazmc@gmail.com>
2023-12-29 09:28:46 -0300

    build project

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -15,24 +15,35 @@
 # echo ..............................installin nvm...
 # curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash 
 
-echo ..............................installin Node whit NVM...
-export NVM_DIR=$HOME/.nvm;
-source $NVM_DIR/nvm.sh;
-source ~/.bashrc
-nvm install node -y
-nvm install 16.20.2
-nvm use 16.20.2
 
-# echo ..............................install Node...
-# curl -sL https://deb.nodesource.com/setup_20.x | sudo -E bash -
-# sudo apt install -y nodejs
+# NODE JS
 
-# echo ..............................install Npm...
-# sudo apt install -y npm
+# Define the expected Node.js version
+# expected_version="14.17.0"
+# install_node(){
+#     echo ..............................installin Node whit NVM...
+#     export NVM_DIR=$HOME/.nvm;
+#     source $NVM_DIR/nvm.sh;
+#     source ~/.bashrc
+#     nvm install node -y
+#     nvm install 16.20.2
+#     nvm use 16.20.2
+#     echo ..............................Version nodejs...
+#     node -v
+#     echo ..............................Version npm...
+#     npm -v  
+# }
 
-echo ..............................Version nodejs...
-node -v
+# Check if Node.js is not installed or version differs
+# if ! command -v node &> /dev/null; then
+#     install_node
+# else
+#     # Get the installed Node.js version
+#     installed_version=$(node -v | cut -c 2-)  # Remove the 'v' from the version
 
-echo ..............................Version npm...
-npm -v
+#     # Compare the installed version with the expected version
+#     if [ "$installed_version" != "$expected_version" ]; then
+#         install_node
+#     fi
+# fi
 
